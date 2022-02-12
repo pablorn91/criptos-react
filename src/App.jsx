705c02becd1e38f3c3ea8c3e7279045db7841195
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-02-12 11:16:37 -0400

    Primeros pasos con Styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+    max-width: 900px;
+    width: 90%;
+    margin: 0 auto;
+    @media (min-width: 992px){
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 2rem;
+    }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+`
+
 
 function App() {
 
   return (
-    <>
-      <h1>Desde App</h1>
-    </>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='Imagenes Criptomonedas'
+      />
+      <Heading>Desde App</Heading>
+    </Contenedor>
   )
 }
 
